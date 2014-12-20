8ebed70fe8b608cb7275a85d07b454a319b7b978
dorsha <doron@dorsha.com>
2014-12-21 01:27:48 +0200

    First commit for dorsha website

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dorshaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dorshaApp
+ */
+angular.module('dorshaApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dorshaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dorshaApp
+ */
+angular.module('dorshaApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
