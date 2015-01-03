9ac9b9096683417709a65b6841d0ef50f8af45fe
dorsha <doron@dorsha.com>
2015-01-03 17:25:03 +0200

    The awesome site!

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,33 +1,21 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name dorshaApp
- * @description
- * # dorshaApp
- *
- * Main module of the application.
- */
 angular
-  .module('dorshaApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('dorshaApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl : 'views/main.html',
+                controller : 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo : '/'
+            });
+    });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,19 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name dorshaApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the dorshaApp
- */
 angular.module('dorshaApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope, $rootScope) {
+
+        $rootScope.messages = {
+            header: ['^1500Hi^1500', 'My name is Doron Sharon^1500', 'You can call me dorsha']
+        };
+
+        $scope.type = function (elemendId, commands, loop, typeSpeed) {
+            $('#' + elemendId + '-cursor').empty();
+            $('#' + elemendId).typed({
+                strings: commands,
+                typeSpeed: typeSpeed || 30,
+                loop: loop || false,
+                backSpeed: 5
+            });
+        };
+    });
