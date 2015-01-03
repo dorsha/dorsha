f8f650532d255b33d55c67ded99add00b1bd0d13
dorsha <doron@dorsha.com>
2015-01-03 19:28:25 +0200

    Removed hash

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,7 +9,7 @@ angular
         'ngSanitize',
         'ngTouch'
     ])
-    .config(function ($routeProvider) {
+    .config(function ($routeProvider, $locationProvider) {
         $routeProvider
             .when('/', {
                 templateUrl : 'views/main.html',
@@ -18,4 +18,5 @@ angular
             .otherwise({
                 redirectTo : '/'
             });
+        $locationProvider.html5Mode(true);
     });
