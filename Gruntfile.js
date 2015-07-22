cd82e89e612719b9607d2598a21aa7c0b164a241
dorsha <doron@dorsha.com>
2015-07-23 00:37:41 +0300

    fixed grunt for travis

--- Gruntfile.js
+++ Gruntfile.js
@@ -472,13 +472,12 @@ module.exports = function (grunt) {
     ]);
 
     grunt.registerTask('default', [
-        'newer:jshint',
-        'test',
         'build'
     ]);
 
 	grunt.registerTask('testBuildAndPublish', [
-        'default',
+        'newer:jshint',
+        'test',
 		'buildAndPublish'
     ]);
 	
